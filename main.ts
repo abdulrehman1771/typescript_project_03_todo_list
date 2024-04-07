dbf23d51c115f4c9ca3004a91d521795b62481f5
abdulrehman1771 <hhatori476@gmail.com>
2024-04-07 13:50:00 +0500

    Added new feature 'Update Task'

--- main.ts
+++ main.ts
@@ -10,7 +10,13 @@ while (true) {
       name: "add",
       type: "list",
       message: "What do you want to do?",
-      choices: ["Add Task", "Remove Task", "View Tasks", "Cancel"],
+      choices: [
+        "Add Task",
+        "Remove Task",
+        "View Tasks",
+        "Update Task",
+        "Cancel",
+      ],
     },
   ]);
 
@@ -21,12 +27,11 @@ while (true) {
       message: "Name the task below",
     });
     lst.push(addition.tsk);
-    console.log('Task added successfully.');
-    
+    console.log("Task added successfully.");
   } else if (task.add === "Remove Task") {
     let remove = await inquirer.prompt({
       name: "rmv",
-      // type:'',
+      // type:"",
       message: "Name the task below",
     });
     if (lst.includes(remove.rmv)) {
@@ -40,7 +45,33 @@ while (true) {
       console.log("Incorrect name.");
     }
   } else if (task.add === "View Tasks") {
-    console.log(lst);
+    lst.forEach((element) => {
+      console.log(element);
+    });
+  } else if (task.add === "Update Task") {
+    let from_val = await inquirer.prompt({
+      name: "from",
+      // type:'string',
+      message: "Enter the name of task you want to update",
+    });
+
+    if (lst.includes(from_val.from)) {
+      let to_val = await inquirer.prompt({
+        name: "to",
+        // type:'',
+        message: "Enter new task name",
+      });
+
+      for (let element = 0; element < lst.length; element++) {
+        if (lst[element] === from_val.from) {
+          lst[element] = to_val.to;
+          console.log("Task updated successfully!");
+          break;
+        }
+      }
+    } else {
+      console.log("Couldn't find task. Please enter correct task name.");
+    }
   } else if (task.add === "Cancel") {
     break;
   }
